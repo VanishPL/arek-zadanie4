c3cb5ecba2d13ce10331d27af31c563849d2d29f
Piotr Rogowski <209446@edu.p.lodz.pl>
2019-02-09 01:16:53 +0100

    Dodano zawijanie i przewijanie tekstu.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JFrame f=new JFrame();//creating instance of JFrame
+
+
+
+
+        f.setSize(800,600);//400 width and 500 height
+        f.setLayout(null);//using no layout managers
+        f.setVisible(true);//making the frame visible
+        JTextArea n = new JTextArea();
+        n.setBounds(100,0,699,599);
+        f.add(n);
+
     }
 }
